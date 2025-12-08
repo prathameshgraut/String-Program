7f2a3bacc495115623d3ba7755ed52b147b6ff30
Prathamesh Raut. <prathamesh1687@gmail.com>
2025-12-08 11:29:38 +0530

    Add files via upload

---
+++ CheckVowels.java
@@ -0,0 +1,22 @@
+package StringCode;
+
+import java.util.Scanner;
+
+public class CheckVowels {
+
+	public static void main(String[] args) {
+		String str="";
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter A String: ");
+		str=sc.next();
+		String res="";
+		for(int i=0;i<str.length();i++) {
+			char ch=str.charAt(i);
+			if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U') {
+				res=res+ch+" ";
+			}
+		}
+		System.out.println("Total Vowels Of String :"+res);
+	}
+
+}
